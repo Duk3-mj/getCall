95c55171c06b2cd1f850cb07514da36b4a0e8a23
Duk3 <jauharimohit2@gmail.com>
2021-12-07 12:06:37 +0530

    desired output

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
+import { getTopStories } from "./api/Toiservices"
 import './App.css';
 
 function App() {
+
+  const [json, setJson] = React.useState()
+  const [top5Stories, setTop5Stories] = React.useState()
+
+  React.useEffect(() => {
+    getTopStories()
+      .then((res) => res.json()
+        .then((json) => {
+          setJson(json.result.slots)
+        })
+      ).catch(err => alert("unable to fetch data" + err))
+  }, [])
+
+  React.useEffect(() => {
+   
+   if(json !== undefined){
+    let output = []
+     json.forEach(item => {
+       let title = item.story.content.title;
+       let link =item.story.content.url;
+       output.push({title,link})
+     });
+     setTop5Stories(output)
+   }
+  },[json])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {top5Stories !== undefined && console.log(top5Stories)}
     </div>
   );
 }
---
+++ src/api/Toiservices.js
@@ -0,0 +1,4 @@
+
+export const getTopStories = () => {
+    return fetch("https://api.spokenlayer.net/web-player/playlist?channel=time-com&_v=alpha&n=9")
+} 
